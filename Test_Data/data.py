d39594a8effea4cbe04defc6a54d54a6a5991ae6
Aman-F8 <amanullahf8@gmail.com>
2025-04-30 15:01:15 +0530

    Add files via upload

---
+++ Test_Data/data.py
@@ -0,0 +1,12 @@
+
+"""
+This file contains all the data used
+"""
+
+class Data:
+
+    url = "https://www.guvi.in"
+    expected_title = "GUVI | Learn to code in your native language"
+    login_url = url + "/sign-in"
+    username = "amanullahf8@gmail.com"
+    password = "Aman@1995"
